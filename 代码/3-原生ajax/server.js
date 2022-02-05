56ed80f7c6e743740a8d8fdda3513a2ec07447c8
emmmwang <954544828@qq.com>
2022-02-06 01:30:13 +0800

    axios 模拟数据库增加

--- 代码/3-原生ajax/server.js
+++ 代码/3-原生ajax/server.js
@@ -6,9 +6,14 @@ const express = require('express');
 //2.创建应用对象
 const app = express();
 
-//3.创建路由规则
-//request 是对请求报文的封装
-//response 是对响应报文的封装
+var bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+        extended: true
+    }))
+    //3.创建路由规则
+    //request 是对请求报文的封装
+    //response 是对响应报文的封装
 app.get('/server', (request, response) => {
     //设置响应头  设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*')
@@ -60,7 +65,7 @@ app.all('/getscript', (request, response) => {
     let str = JSON.stringify(data);
 
     let scriptStr = request.query.callback + `(${str})`;
-    // response.sent 发送给客户端, 客户端去把这个字符串,当作JS迪马去解析执行
+    // response.sent 发送给客户端, 客户端去把这个字符串,当作JS去解析执行
 
     //设置响应体
     response.send(scriptStr)
@@ -81,11 +86,47 @@ app.all('/axios-server', (request, response) => {
     //设置响应头  设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.setHeader('Access-Control-Allow-Headers', '*')
+    let par = request.query
+        //设置响应体
+    const data = { name: "蒋莹莹" }
+    let res = JSON.stringify(data) + JSON.stringify(par)
+    response.send(res)
+});
+
+app.all('/axios-vue', (request, response) => {
+    //设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
 
     //设置响应体
-    const data = { name: "蒋莹莹" }
-    response.send(JSON.stringify(data))
+    let result = [
+        { id: 1, name: "奥迪", ctime: new Date() },
+        { id: 2, name: "宝马", ctime: new Date() },
+        { id: 3, name: "奔驰", ctime: new Date() }
+    ]
+
+
+    // 因为没有连接数据库,所以把前端显示的数据一起传过来更新
+    // 当传入参数的时候 默认为post方法, 添加数据
+    if (request.body.name) {
+        // 获取前端显示的数据
+        result = JSON.parse(request.body.list);
+        console.log(result);
+        // id 必须定义在里面,不然每次都是原始数据长度+1 ----4
+        let id = result.length + 1
+        result.push({ id: id, name: request.body.name, ctime: new Date() })
+        let str = JSON.stringify(result)
+        response.end(str)
+    } else {
+        // 第一次直接返回数据
+        let str = JSON.stringify(result)
+
+        response.end(str)
+    }
+
+
 });
+
 
 
 app.all('/jquery-jsonp-server', (request, response) => {
@@ -98,6 +139,10 @@ app.all('/jquery-jsonp-server', (request, response) => {
     let cb = request.query.callback;
     response.end(`${cd}(${str})`)
 });
+
+
+
+
 
 
 //4.监听端口启动服务
